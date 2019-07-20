9dcb64eb8eeed867d6c66c95174932ff14b50f32
Anthony <cozziad@gmail.com>
2019-07-19 22:45:03 -0400

    Lecture 171 Done

--- vision-app-dev/Controller/CameraVC.swift
+++ vision-app-dev/Controller/CameraVC.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
 
+    //Outlets
     @IBOutlet weak var captureImageView: RoundedShadowImageView!
     @IBOutlet weak var flashBtn: RoundedShadowButton!
     @IBOutlet weak var IDLbl: UILabel!
@@ -17,11 +19,72 @@ class CameraVC: UIViewController {
     @IBOutlet weak var cameraView: RoundedShadow!
     @IBOutlet weak var roundedShadowView: RoundedShadow!
     
+    //Vars
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    var photoData: Data?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer.frame = cameraView.bounds
+    }
+    
+   
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapCameraView))
+        tap.numberOfTapsRequired = 1
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
+        
+        let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        do {let input = try AVCaptureDeviceInput(device: backCamera!)
+            if captureSession.canAddInput(input){captureSession.addInput(input)}
+            
+            cameraOutput = AVCapturePhotoOutput()
+            if captureSession.canAddOutput(cameraOutput){captureSession.addOutput(cameraOutput!)}
+            
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+            previewLayer.connection?.videoOrientation = .portrait
+            cameraView.addGestureRecognizer(tap)
+            cameraView.layer.addSublayer(previewLayer!)
+            captureSession.startRunning()
+        }
+        catch{}
+    }
+    
+    @objc func didTapCameraView(){
+        let settings = AVCapturePhotoSettings()
+//        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+//        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType, kCVPixelBufferWidthKey as String: 160, kCVPixelBufferHeightKey as String :160]
+//
+        
+        settings.previewPhotoFormat = settings.embeddedThumbnailPhotoFormat
+        
+        cameraOutput.capturePhoto(with: settings, delegate: self)
+    }
+}
 
-
+extension CameraVC: AVCapturePhotoCaptureDelegate{
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error{
+            debugPrint(error)
+            return
+        }
+        photoData = photo.fileDataRepresentation()
+        let image = UIImage(data: photoData!)
+        self.captureImageView.image = image
+    }
 }
 
